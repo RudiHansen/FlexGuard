e836a735d6411e5e9eab743331044faab6747300
Rudi Hansen <smid>
2025-07-25 09:01:11 +0200

    Core: Add VersionHelper for handling Version Number from Directory.Build.props

---
+++ FlexGuard.Core/Util/VersionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace FlexGuard.Core.Util;
+
+public static class VersionHelper
+{
+    public static string GetAppVersion()
+    {
+        var version = Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion ?? "unknown";
+        var plusIndex = version.IndexOf('+');
+        return plusIndex >= 0 ? version[..plusIndex] : version;
+    }
+}
